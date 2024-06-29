cb331c6f2a722b419400190b2f0b7b13e0a2e134
Robert <z93hpjlr@duck.com>
2024-06-29 13:37:56 +0200

    Alle Klassen für Aufgabe 1

---
+++ Uebungen 3/src/Bestellposition.java
@@ -0,0 +1,14 @@
+public class Bestellposition {
+    private Ware ware;
+    private int menge;
+    public Bestellposition(Ware ware, int menge) {
+        this.ware = ware;
+        this.menge = menge;
+    }
+    public Ware getWare() {
+        return ware;
+    }
+    public int getMenge() {
+        return menge;
+    }
+}
---
+++ Uebungen 3/src/Bestellung.java
@@ -0,0 +1,21 @@
+public class Bestellung {
+    private final Bestellposition[] positionen = new Bestellposition[100];
+
+    public Bestellung() {
+
+    }
+
+    public void nimmAuf(Bestellposition position) {
+        int lastIndex = 0;
+        while (positionen[lastIndex] != null) {
+            lastIndex++;
+        }
+        positionen[lastIndex] = position;
+    }
+
+    public void zeigeAn() {
+        for (Bestellposition b : positionen) {
+            System.out.println(b);
+        }
+    }
+}
